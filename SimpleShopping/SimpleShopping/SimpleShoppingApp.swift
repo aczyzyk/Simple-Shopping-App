0e75ac6f7f35f8ec2f40de895d497a11a8456e46
AC <siskinsoft@icloud.com>
2024-10-08 11:50:49 +0200

    Initial project files

---
+++ SimpleShopping/SimpleShopping/SimpleShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleShoppingApp.swift
+//  SimpleShopping
+//
+//  Created by AC on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
